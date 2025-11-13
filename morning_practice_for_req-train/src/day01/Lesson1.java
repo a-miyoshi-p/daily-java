7e62b4e812ce71b4a150f5f4a7c6e6685070cda8
edu <edu@WIN-P4HS74PTE6L.lan>
2025-11-13 10:03:46 +0900

    1113_morning_practice_for_req-train_day1_answer

--- morning_practice_for_req-train/src/day01/Lesson1.java
+++ morning_practice_for_req-train/src/day01/Lesson1.java
@@ -21,6 +21,23 @@ public class Lesson1 {
 
 	public static void main(String[] args) {
 		/*ここから記入*/
+		System.out.println("出力します");
+
+		int num1 = 12;
+//		byte num1 = 12;
+//		short num1 = 12;
+		System.out.println(num1);
+
+		double num2 = 1.6;
+//		float num2 = 1.6;→型の不一致: double から float には変換できません
+		System.out.println(num2);
+		
+//		こんにちはを代入する
+		String hello = "こんにちは";
+		System.out.println(hello);
+
+		boolean quiz = true;
+		System.out.println(quiz);
 
 	}
 

dc075992a46edd8f26e0e313fb47ae0b9be3f804
edu <edu@WIN-P4HS74PTE6L.lan>
2025-11-28 12:58:30 +0900

    day10_q02済

--- morning_practice_for_req-train/src/day10/q02/Main.java
+++ morning_practice_for_req-train/src/day10/q02/Main.java
@@ -31,35 +31,45 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		System.out.println("1から任意の値までの合計を求めます");
 		System.out.println("input num? >>");
-		
+		//入力値の取得
 		String str = br.readLine();
-		int num = Integer.parseInt(str);
-		//リスト作成
-		List<Integer> numbers = new ArrayList<>();
-		//入力値までの要素を追加
-		for (int i = 1; i <= num; ++i) {
-			numbers.add(i);
-		}
-		//各要素を取り出す
-		for(int value:numbers) {
-			System.out.println(value);
-//			int sum =numbers.get(value);
-//			System.out.println(sum);
-		}
-		System.out.println("1から"+num+"までの合計は");
-		System.out.println("listの要素を表示します");
-		System.out.println(numbers);
-		//※入力値に整数以外が含まれていた場合、NumberFormatExceptionをキャッチし例外処理を行うこと
-		/*try {
-			
+
+		try {
+			//整数型にキャスト
+			//※入力値に整数以外が含まれていた場合、NumberFormatExceptionをキャッチし例外処理を行うこと
+			int num = Integer.parseInt(str);
+
+			//合計値計算用変数宣言
+			int sum = 0;
+			//リスト作成
+			List<Integer> numbers = new ArrayList<>();
+			//入力値までの要素を追加
+			for (int i = 1; i <= num; ++i) {
+				numbers.add(i);
+			}
+			//要素の総和計算
+			//代替例）int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+			for (int i : numbers) {
+				sum += i;
+			}
+			//各要素を取り出す
+			System.out.println("1から" + num + "までの合計は" + sum);
+			System.out.println("listの要素を表示します");
+			for (int value : numbers) {
+				System.out.println(value);
+			}
 		} catch (NumberFormatException e) {
-			// TODO: handle exception
-		}*/
+			e.getStackTrace();
+			System.out.println("整数を入力してください");
+		}
+		//53,58は共通化
+		//
 	}
 
 }
